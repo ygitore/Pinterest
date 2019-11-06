7b52447ce2f913204ba9af2457d44d53bf1b8698
Yitbarek Gitore <yitbareky@gmail.com>
2019-11-06 13:43:59 -0600

    added top navbars

--- src/javascripts/helpers/data/authData.js
+++ src/javascripts/helpers/data/authData.js
@@ -7,20 +7,27 @@ import './authData.scss';
 const authDiv = $('#auth');
 const stockDiv = $('#stock');
 const logoutNavbar = $('#navbar-button-logout');
-
+const home = $('#navbar-button-home');
+const following = $('#navbar-button-following');
+const dropdownList = $('#navbarDropdownMenuLink');
 const checkLoginStatus = () => {
   firebase.auth().onAuthStateChanged((user) => {
     if (user) {
       // someone is logged in - we should NOT see auth component
       stockDiv.removeClass('hide'); // when user login, we dont see stockDiv
       logoutNavbar.removeClass('hide'); // when user login, we hide class will be removed
+      home.removeClass('hide');
+      following.removeClass('hide');
+      dropdownList.removeClass('hide');
       authDiv.addClass('hide');
-      $('<input id ="mySearch" class = "mySearch" type = "text" placeholder = "Search">').insertAfter('.navbar-brand');
       $('#exampleModalCenter').modal('show');
     } else {
       // nobody logged in SHOW auth component
       stockDiv.addClass('hide');
       logoutNavbar.addClass('hide');
+      home.addClass('hide');
+      dropdownList.addClass('hide');
+      following.addClass('hide');
       authDiv.removeClass('hide');
       $('.mySearch').hide();
     }
