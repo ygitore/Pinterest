d5cc5d27c7c47161f1a7a21b226adbd05b167285
Yitbarek Gitore <yitbareky@gmail.com>
2019-11-09 11:57:55 -0600

    fixed displaying boards and pins

--- src/javascripts/components/boardsCard/boardsCard.js
+++ src/javascripts/components/boardsCard/boardsCard.js
@@ -1,17 +1,25 @@
-import boards from '../../helpers/data/boardsData';
+import boardsData from '../../helpers/data/boardsData';
+import pins from '../pins/pins';
 import utils from '../../helpers/utilities';
 
-const buildBoards = () => {
-  boards.getBoardData()
-    .then((boardId) => {
+
+const myButtonClick = (e) => {
+  pins.buildPins(e.target.id);
+};
+const buildBoards = (uid) => {
+  boardsData.getBoardData(uid)
+    .then((allBoards) => {
       let domString = '<h2>The boards</h2>';
       domString += '<div class="d-flex flex-wrap justify-content-between">';
-      boardId.forEach((board) => {
-        domString += `<a class="card btn btn-light" id ="board-${board.name}">${board.name}</a>`;
+      allBoards.forEach((board) => {
+        domString += `<button class="card btn btn-light" id ="${board.id}">${board.name}</button>`;
       });
       domString += '</div>';
       utils.printToDom('machine', domString);
+      allBoards.forEach((x) => {
+        document.getElementById(`${x.id}`).addEventListener('click', myButtonClick);
+      });
     })
-    .catch((err) => console.log(err));
+    .catch((err) => console.error(err));
 };
 export default { buildBoards };
